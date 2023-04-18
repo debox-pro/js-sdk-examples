3a612c5fda3224e60b968e24bc8204753ec33256
kringt <364734461@qq.com>
2023-04-18 13:09:40 +0800

    init

--- html/main.js
+++ html/main.js
@@ -1,32 +1,32 @@
-import { DeBoxChat } from '@debox-global/html'
+import { DeBoxChat } from "@debox-global/html";
 
 function getQueryVariable(variable) {
-  const query = window.location.search.substring(1)
-  const vars = query.split('&')
+  const query = window.location.search.substring(1);
+  const vars = query.split("&");
   // eslint-disable-next-line @typescript-eslint/prefer-for-of, no-plusplus
   for (let i = 0; i < vars.length; i++) {
-    const pair = vars[i].split('=')
+    const pair = vars[i].split("=");
     if (pair[0] === variable) {
-      return pair[1]
+      return pair[1];
     }
   }
 
-  return false
+  return false;
 }
 
 window.deboxChat = new DeBoxChat(
   {
     dao: {
       contractAddress:
-        getQueryVariable('contractAddress') || '0xb072a444a7afc206e7898660be7372153299a84b',
-      chainId: getQueryVariable('chainId') || 1,
-      source: getQueryVariable('source') || 0
+        getQueryVariable("contractAddress") || import.meta.env.VITE_CA,
+      chainId: getQueryVariable("chainId") || import.meta.env.VITE_CID,
+      source: getQueryVariable("source") || import.meta.env.VITE_SOURCE,
     },
-    projectId: getQueryVariable('projectId') || import.meta.env.VITE_PKEY,
+    projectId: getQueryVariable("projectId") || import.meta.env.VITE_PKEY,
     urls: {
-      api: getQueryVariable('api') || import.meta.env.VITE_API,
-      ws: getQueryVariable('ws') || import.meta.env.VITE_WS
-    }
+      api: getQueryVariable("api") || import.meta.env.VITE_API,
+      ws: getQueryVariable("ws") || import.meta.env.VITE_WS,
+    },
   },
   window.ethereum // with inject wallet
-)
+);
